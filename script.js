94c34ed9402ab6e787af4e4114da3878f7ea74d0
hogexy <43495681+hogexy@users.noreply.github.com>
2023-06-04 00:20:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
 /* script.js */
 
-// ボタンをクリックしたときの画像のシフト動作を実装します
-document.getElementById('button1').addEventListener('click', function() {
-    const gallery = document.getElementById('gallery');
-    const images = Array.from(gallery.getElementsByClassName('gallery-image'));
-    const firstImageSrc = images[0].src;
+window.onload = function() {
+    var images = [
+        "https://meek-cactus-94d5bb.netlify.app/Dunkleosteus(1).jpg",
+        "https://meek-cactus-94d5bb.netlify.app/Dunkleosteus(2).jpg",
+        "https://meek-cactus-94d5bb.netlify.app/Dunkleosteus(3).jpg"
+    ];
+    var currentIndex = 0;
 
-    // 左シフト
-    for (let i =0; i < images.length - 1; i++) {
-        images[i].src = images[i + 1].src;
+    function updateImages() {
+        document.getElementById('img1').src = images[currentIndex % images.length];
+        document.getElementById('img2').src = images[(currentIndex + 1) % images.length];
+        document.getElementById('img3').src = images[(currentIndex + 2) % images.length];
     }
 
-    images[images.length - 1].src = firstImageSrc;
-});
-
-document.getElementById('button2').addEventListener('click', function() {
-    const gallery = document.getElementById('gallery');
-    const images = Array.from(gallery.getElementsByClassName('gallery-image'));
-    const lastImageSrc = images[images.length - 1].src;
-
-    // 右シフト
-    for (let i = images.length - 1; i > 0; i--) {
-        images[i].src = images[i - 1].src;
-    }
-
-    images[0].src = lastImageSrc;
-});
-
-// 画像をクリックしたときの大きく表示と元に戻る動作を実装します
-const galleryImages = Array.from(document.getElementsByClassName('gallery-image'));
-galleryImages.forEach(image => {
-    image.addEventListener('click', function() {
-        if (this.className === 'gallery-image') {
-            this.className = 'gallery-image enlarged';
-        } else {
-            this.className = 'gallery-image';
-        }
+    document.getElementById('shift-left').addEventListener('click', function() {
+        currentIndex--;
+        updateImages();
     });
-});
+
+    document.getElementById('shift-right').addEventListener('click', function() {
+        currentIndex++;
+        updateImages();
+    });
+
+    var modal = document.createElement('div');
+    modal.style.display = 'none';
+    modal.style.position = 'fixed';
+    modal.style.top = '50%';
+    modal.style.left = '50%';
+    modal.style.transform = 'translate(-50%, -50%)';
+    modal.style.maxWidth = '90%';
+    modal.style.maxHeight = '90%';
+    document.body.appendChild(modal);
+
+    function showModal(imgSrc) {
+        var img = document.createElement('img');
+        img.src = imgSrc;
+        img.style.maxWidth = '100%';
+        img.style.maxHeight = '100%';
+        modal.appendChild(img);
+        modal.style.display = 'block';
+    }
+
+    function hideModal() {
+        modal.innerHTML = '';
+        modal.style.display = 'none';
+    }
+
+    modal.addEventListener('click', hideModal);
+
+    document.getElementById('img1').addEventListener('click', function() {
+        showModal(this.src);
+    });
+
+    document.getElementById('img2').addEventListener('click', function() {
+        showModal(this.src);
+    });
+
+    document.getElementById('img3').addEventListener('click', function() {
+        showModal(this.src);
+    });
+}
+
